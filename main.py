23e661dcdd23f8fb1aa627e81be7faccc3bb0eb4
radzsza <aleksandraradziukiewicz@gmail.com>
2025-06-25 22:03:24 +0200

    finished backend

--- main.py
+++ main.py
@@ -1,60 +1,85 @@
-import openmeteo_requests
-import math
-import pandas as pd
-import requests_cache
-from retry_requests import retry
+from fastapi import FastAPI, Query, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+import requests
 
-# Setup the Open-Meteo API client with cache and retry on error
-cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
-retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
-openmeteo = openmeteo_requests.Client(session = retry_session)
+app = FastAPI()
 
-lon = 19.56
-lat = 50.04
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-# Make sure all required weather variables are listed here
-# The order of variables in hourly or daily is important to assign them correctly below
-url = "https://api.open-meteo.com/v1/forecast"
-params = {
-	"latitude": lat,
-	"longitude": lon,
-	"daily": ["weather_code", "temperature_2m_max", "temperature_2m_min", "sunshine_duration"],
-	"timezone": "auto"
-}
-responses = openmeteo.weather_api(url, params=params)
+PV_POWER = 2.5
+PV_EFFICIENCY = 0.2
 
-# Process first location. Add a for-loop for multiple locations or weather models
-response = responses[0]
-print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
-print(f"Elevation {response.Elevation()} m asl")
-print(f"Timezone {response.Timezone()}{response.TimezoneAbbreviation()}")
-print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+def get_url(lat, lon):
+	url_base = "https://api.open-meteo.com/v1/forecast?"
+	params = [f"latitude={lat}",
+		f"longitude={lon}",
+		"daily="
+	]
 
-# Process daily data. The order of variables needs to be the same as requested.
-daily = response.Daily()
-daily_weather_code = daily.Variables(0).ValuesAsNumpy()
-daily_temperature_2m_max = daily.Variables(1).ValuesAsNumpy()
-daily_temperature_2m_min = daily.Variables(2).ValuesAsNumpy()
-daily_sunshine_duration_hours = daily.Variables(3).ValuesAsNumpy()/3600
-daily_generated_power = daily_sunshine_duration_hours * 2.5 * 0.2
+	daily_params = [
+		"weather_code",
+		"temperature_2m_max",
+		"temperature_2m_min",
+		"sunshine_duration",
+		"surface_pressure_mean"
+	]
 
-daily_data = {"date": pd.date_range(
-	start = pd.to_datetime(daily.Time(), unit = "s", utc = True),
-	end = pd.to_datetime(daily.TimeEnd(), unit = "s", utc = True),
-	freq = pd.Timedelta(seconds = daily.Interval()),
-	inclusive = "left"
-)}
+	return(url_base + "&".join(params) + ",".join(daily_params))
 
-print(f"Mean sunshine duration is {daily_sunshine_duration_hours.mean()} hours")
-print(f"Mean generated power is {daily_generated_power.mean()} kW")
-print(f"Max temperature is {daily_temperature_2m_max.max()} C")
-print(f"Min temperature is {daily_temperature_2m_min.min()} C")
+def get_forecast_data(lat, lon):
+	url = get_url(lat, lon)
+	response = requests.get(url)
+	return response.json()
 
-daily_data["weather_code"] = daily_weather_code
-daily_data["temperature_2m_max"] = daily_temperature_2m_max
-daily_data["temperature_2m_min"] = daily_temperature_2m_min
-daily_data["sunshine_duration_hours"] = daily_sunshine_duration_hours
-daily_data["generated_power"] = daily_generated_power
 
-daily_dataframe = pd.DataFrame(data = daily_data)
-print(daily_dataframe)
+@app.get("/forecast/daily")
+def get_forecast(lat: float = Query(...), lon: float = Query(...)):
+    if not is_valid_geolocation(lat, lon):
+        raise HTTPException(status_code=400)
+    
+    forecast_data = get_forecast_data(lat, lon)
+    energy = get_pv_energy(forecast_data["daily"]["sunshine_duration"])
+    forecast_data["daily"]["energy"] = energy
+    forecast_data["daily_units"]["energy"] = "kWh"
+    
+    return forecast_data
+
+@app.get("/forecast/summary")
+def get_summary(lat: float = Query(...), lon: float = Query(...)):
+    if not is_valid_geolocation(lat, lon):
+        raise HTTPException(status_code=400)
+    
+    forecast_data = get_forecast_data(lat, lon)
+    forecast_data["weather_summary"] = is_rainy_week(forecast_data["daily"]["weather_code"])
+    forecast_data["temperature_min"] = min(forecast_data["daily"]["temperature_2m_min"])
+    forecast_data["temperature_max"] = max(forecast_data["daily"]["temperature_2m_max"])
+    forecast_data["avg_surface_pressure"] = round(sum(forecast_data["daily"]["surface_pressure_mean"])/7, 2)
+    forecast_data["avg_sunshine_duration"] = round(sum(forecast_data["daily"]["sunshine_duration"])/7/3600, 2)
+    
+    return forecast_data
+
+def is_valid_geolocation(lat, lon):
+	try:
+		lat = float(lat)
+		lon = float(lon)
+	except (ValueError, TypeError):
+		return False
+	return lat in range(-90, 91) and lon in range(-180, 181)
+    
+def is_rainy_week(weather_codes):
+    i = 0
+    for wc in weather_codes:
+        if wc >= 51:
+            i += 1
+    return "z opadami" if i>=4 else "bez opadów"
+
+def get_pv_energy(sunshine_array):
+    pv_energy = []
+    for sd in sunshine_array:
+        pv_energy.append(PV_EFFICIENCY * PV_POWER * sd/3600)
+    return pv_energy
